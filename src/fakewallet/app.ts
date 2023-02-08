a1a87230c13eecfedfbe5125f888ebd44b1421ea
Your Name <you@example.com>
2023-02-08 03:37:25 +0000

    dev

--- src/fakewallet/app.ts
+++ src/fakewallet/app.ts
@@ -1,3 +1,4 @@
+import * as ethers from "ethers";
 export async function onqrcodescan(app: any, data: any) {
     const ok = await onuripaste(app, data);
     if (ok) {
@@ -6,10 +7,16 @@ export async function onqrcodescan(app: any, data: any) {
 }
 export async function onuripaste(app: any, data: any): Promise<boolean> {
     const uri = typeof data === "string" ? data : "";
-    if (uri) {
-        await app.setState({ uri });
-        await app.initWalletConnect();
-        return true;
+    switch (true) {
+        case uri.startsWith('wc:'):
+            await app.setState({ uri });
+            await app.initWalletConnect();
+            return true;
+        case ethers.utils.isAddress(uri):
+            // TODO
+            console.log('address');
+            return false
+        default:
+            return false
     }
-    return false;
 }
