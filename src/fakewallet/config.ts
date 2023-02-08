be684e4bf05e24d68b6eba5171ec82cb9bd370c4
Your Name <you@example.com>
2023-02-08 02:41:38 +0000

    dev

--- src/fakewallet/config.ts
+++ src/fakewallet/config.ts
@@ -1,4 +1,12 @@
 import { IAppConfig } from "../helpers/types";
+import { getLocal, setLocal } from "../helpers/local";
 export function loadconfig(def: IAppConfig): IAppConfig {
+    const localdata = getLocal('__fakewallet__') as any[];
+    if (localdata) {
+        def.numberOfAccounts = localdata.length;
+    } else {
+        setLocal('__fakewallet__', [])
+        def.numberOfAccounts = 0;
+    }
     return def;
 }
