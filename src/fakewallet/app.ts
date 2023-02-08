1a841e7c573908555efe5100eb7a213d8fbacbf2
Your Name <you@example.com>
2023-02-08 05:48:21 +0000

    dev

--- src/fakewallet/app.ts
+++ src/fakewallet/app.ts
@@ -17,22 +17,41 @@ export async function onuripaste(app: App, data: any): Promise<boolean> {
     const uri = typeof data === "string" ? data : "";
     switch (true) {
         case uri.startsWith('wc:'):
-            await app.setState({ uri });
-            await app.initWalletConnect();
-            return true;
-        case ethers.utils.isAddress(uri):
-            const address = ethers.utils.getAddress(uri);
-            if (app.state.accounts.includes(address)) {
-                alert('account already exist');
+            {
+                await app.setState({ uri });
+                await app.initWalletConnect();
                 return true;
             }
-            const localdata = getLocal('__fakewallet__') as string[];
-            localdata.push(address);
-            setLocal('__fakewallet__', localdata);
-            const activeIndex = app.state.accounts.length;
-            const accounts = app.state.accounts.concat([address]);
-            app.setState({ accounts, address, activeIndex });
-            return true;
+        case ethers.utils.isAddress(uri):
+            {
+                const address = ethers.utils.getAddress(uri);
+                if (app.state.accounts.includes(address)) {
+                    alert('account already exist');
+                    return true;
+                }
+                const localdata = getLocal('__fakewallet__') as string[];
+                localdata.push(address);
+                setLocal('__fakewallet__', localdata);
+                const activeIndex = app.state.accounts.length;
+                const accounts = app.state.accounts.concat([address]);
+                app.setState({ accounts, address, activeIndex });
+                return true;
+            }
+        case uri.startsWith('ethereum:') && ethers.utils.isAddress(uri.slice(9)):
+            {
+                const address = ethers.utils.getAddress(uri.slice(9));
+                if (app.state.accounts.includes(address)) {
+                    alert('account already exist');
+                    return true;
+                }
+                const localdata = getLocal('__fakewallet__') as string[];
+                localdata.push(address);
+                setLocal('__fakewallet__', localdata);
+                const activeIndex = app.state.accounts.length;
+                const accounts = app.state.accounts.concat([address]);
+                app.setState({ accounts, address, activeIndex });
+                return true;
+            }
         default:
             return false
     }
